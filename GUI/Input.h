c65d13cc7b1f0e41cf7cbe7c49573f75b8c3ec91
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2018-11-01 20:38:52 +0200

    Update Input.h

--- GUI/Input.h
+++ GUI/Input.h
@@ -4,19 +4,18 @@
 #include "..\DEFS.h"
 #include "UI_Info.h" 
 #include "..\CMUgraphicsLib\CMUgraphics.h"
+#include<iostream> 
+class Output;									// forward declaration
 
-class Output;   // forward declaration of The Class Output
-
-class Input		//The application manager should have a pointer to this class
+class Input										//The application manager should have a pointer to this class
 {
 private:
-	window *pWind;	//Pointer to the Graphics Window
+	window *pWind;								//Pointer to the Graphics Window
 public:
-	Input(window *pW);		//Consturctor
-	void GetPointClicked(int &x, int &y) const;//Get coordinate where user clicks
-	string GetSrting(Output* pO) const ;	 //Returns a string entered by the user
-
-	ActionType GetUserAction() const; //Read the user click and map to an action
+	Input(window *pW);							//Consturctor
+	void GetPointClicked(int &x, int &y) const;	//Get coordinate where user clicks
+	string GetSrting(Output* pO) const ;		//Returns a string entered by the user
+	ActionType GetUserAction() const;			//Read the user click and map to an action
 
 	~Input();
 };
