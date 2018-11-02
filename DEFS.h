98ad84ab6f4c5dd1ec100ee76630e1e94cf36976
Ahmed Ashraf <40968967+aashraf22@users.noreply.github.com>
2018-11-02 04:37:35 +0200

    Figures tool bar Action

--- DEFS.h
+++ DEFS.h
@@ -6,11 +6,15 @@
 //This file contains some global constants and definitions to be used in the project.
 enum ActionType //The actions supported (you can add more if needed)
 {
+	TO_FIGURE,
+	/////////////////           Actions of Figures        ///////////////////
 	DRAW_LINE,		//Draw Line
 	DRAW_RECT,		//Draw Rectangle
 	DRAW_TRI,		//Draw Triangle
-	DRAW_RHOMBUS,		//Draw Rhombus
-	DRAW_ELLIPSE,		//Draw Ellipse
+	DRAW_RHOMBUS,	//Draw Rhombus
+	DRAW_ELLIPSE,	//Draw Ellipse
+	BACK1,
+
 	CHNG_DRAW_CLR,	//Change the drawing color
 	CHNG_FILL_CLR,	//Change the filling color
 	DEL,			//Delete a figure(s)
@@ -18,15 +22,28 @@ enum ActionType //The actions supported (you can add more if needed)
 	SAVE_BY_TYPE,	//Save the all the figures that have a specific type
 	LOAD,			//Load a graph from a file
 	EXIT,			//Exit the application
-
 	DRAWING_AREA,	//A click on the drawing area
 	STATUS,			//A click on the status bar
 	EMPTY,			//A click on empty place in the toolbar
-
 	TO_DRAW,		//Switch interface to Draw mode
-	TO_PLAY			//Switch interface to Play mode
-
+	TO_PLAY,			//Switch interface to Play mode
+	TO_COLOR,
 	///TODO: Add more action types (if needed)
+	PICK_TYPE,     // Pick By Type 
+	PICK_COLOR,	   // Pick By Color
+
+	COPY,
+	CUT,
+	PASTE,
+	DELETE1,
+	
+	/////// Action of Color Mode ///////
+	WHITE1,
+	BLACK1,
+	RED1,
+	GREEN1,
+	BLUE1,
+	BACK
 };
 
 struct Point	//To be used for figures points
